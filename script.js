c53f5228e5fe94f4bcb5e6738c6c63bff8dbb8f4
subin-pp <subinpp2255@gmail.com>
2024-12-01 13:01:17 +0530

    changed js code

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const countryStatus = async ()=>{
+const countryStatus = async () => {
     console.log(userinput.value);
-    if(userinput.value){
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userinput.value}&appid=0949f6f524fa5ee6e5981309bc89fde9&units=metric`)
+    if (userinput.value) {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userinput.value}&appid=0949f6f524fa5ee6e5981309bc89fde9&units=metric`);
         console.log(response);
 
-        if(response.status==200){
-            let WeatherDetails = await response.json()
+        if (response.status == 200) {
+            let WeatherDetails = await response.json();
 
-            
-            result1.innerHTML = ` <div class="date-place">
+            // Convert Celsius to Fahrenheit
+            const tempInCelsius = WeatherDetails.main.temp;
+            const tempInFahrenheit = (tempInCelsius * 1.8) + 32;
+
+            result1.innerHTML = ` 
+                <div class="date-place">
                     <h4 class="place"><i class="fa-solid fa-location-dot"></i> ${WeatherDetails.name}, <span>${WeatherDetails.sys.country}</span></h4>
                 </div>
-                <h1 class="degree"><span id="degree-value">${WeatherDetails.main.temp}</span><sup id="degree-c">°C</sup></h1>
-                <h4 class="mood"><i class="fa-solid fa-cloud"></i>${WeatherDetails.weather[0].main}</h4>`
+                <h1 class="degree">
+                    <span id="degree-value">${tempInFahrenheit.toFixed(2)}</span>
+                    < id="degree-f">°F</
+                </h1>
+                <h4 class="mood"><i class="fa-solid fa-cloud"></i>${WeatherDetails.weather[0].main}</h4>`;
 
-
-                result2.innerHTML = ` <div class="additional-content">
+            result2.innerHTML = ` 
+                <div class="additional-content">
                     <div class="add-items">
-                        <h1></i><span>${WeatherDetails.main.pressure} hPa</span></h1>
+                        <h1><span>${WeatherDetails.main.pressure} hPa</span></h1>
                         <p>PRESSURE</p>
                     </div>
                     <div class="add-items">
@@ -28,18 +35,11 @@ const countryStatus = async ()=>{
                         <h1> <span>${WeatherDetails.main.humidity} %</span></h1>
                         <p>HUMIDITY</p>
                     </div>
-                </div>`
+                </div>`;
         }
-        
-        
-        
-        
-    }else{
-        result1.innerHTML = `<h1 class="degree"><span id="degree-value">Please Enter Place </span><sup id="degree-c">°C</sup></h1>`
-
     }
-    
-}
+};
+
 
 
 userinput.addEventListener('keydown', (event) => {
